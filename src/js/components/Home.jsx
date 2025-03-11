03404a6fda9b0fba087e2f4040f5377b96f6ed6f
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-03-11 22:43:52 +0000

    nuevos archivos

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,23 +6,31 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<nav className="navbar navbar-expand-lg bg-black">
+		<div className="container-fluid">
+		  <a className="navbar-brand text-white" href="#">Start Bootstrap</a>
+		  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span className="navbar-toggler-icon"></span>
+		  </button>
+		  <div className="collapse navbar-collapse" id="navbarNav">
+			<ul className="navbar-nav">
+			  <li className="nav-item">
+				<a className="nav-link active text-white" aria-current="page" href="#">Home</a>
+			  </li>
+			  <li className="nav-item">
+				<a className="nav-link text-white" aria-disabled="true" href="#">About</a>
+			  </li>
+			  <li className="nav-item">
+				<a className="nav-link text-white" aria-disabled="true" href="#">Services</a>
+			  </li>
+			  <li className="nav-item">
+				<a className="nav-link text-white" aria-disabled="true">Contact</a>
+			  </li>
+			</ul>
+		  </div>
 		</div>
+	  </nav>
 	);
-};
+  };
 
 export default Home;
